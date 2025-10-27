5754629cd862bc1314302bdc1487c2f8080ec936
Lexcode <kontakt@lexcode.de>
2025-10-27 12:31:00 +0100

    fix: update BlogPostRepository before switching branch

--- modules/WebsiteBlog/Repositories/BlogPostRepository.php
+++ modules/WebsiteBlog/Repositories/BlogPostRepository.php
@@ -5,7 +5,7 @@ namespace Modules\WebsiteBlog\Repositories;
 use Modules\Website\Repositories\BaseRepository;
 use Modules\WebsiteBlog\Models\BlogPost;
 
-class BlogPostRepository extends BaseRepository
+class BlogPostRepository extends BaseReptository
 {
     public function __construct()
     {
